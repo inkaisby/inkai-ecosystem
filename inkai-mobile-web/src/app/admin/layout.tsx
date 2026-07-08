9ada9ca302a5783f34bb9170f327e157aa33f7e7
karateinkaisby <karateinkaisby@gmail.com>
2026-07-08 12:30:33 +0700

    fix: make admin topbar spacer active on all viewports to prevent covering page content

--- inkai-mobile-web/src/app/admin/layout.tsx
+++ inkai-mobile-web/src/app/admin/layout.tsx
@@ -84,7 +84,7 @@ export default function AdminLayout({
 
         {/* Content Panel */}
         <div className="flex-1 flex flex-col min-w-0 lg:pl-64 w-full">
-          <div className="admin-topbar-spacer lg:hidden" aria-hidden />
+          <div className="admin-topbar-spacer" aria-hidden />
           <TopBar />
           <main className="relative min-w-0 flex-1 flex flex-col px-4 md:px-8 pt-4 pb-32 lg:pb-8 max-w-7xl mx-auto w-full">
             {children}
